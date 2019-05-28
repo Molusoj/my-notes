a4970b76c27dafc900379b56e2dec1f3f19b5dfb
Michael Olusoji <molusoj@gmail.com>
2019-05-28 15:27:10 +0100

    Adding Home function

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notes.apps.NotesConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
